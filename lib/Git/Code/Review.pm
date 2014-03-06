c435d4023ff6e3625440fccafa9c91cfe3ba6ffd
Brad Lhotsky <brad.lhotsky@booking.com>
2014-03-06 21:52:20 +0100

    Initial skeleton for Git::Code::Review

---
+++ lib/Git/Code/Review.pm
@@ -0,0 +1,11 @@
+# ABSTRACT: Tools for performing code review using Git as the backend
+package Git::Code::Review;
+use strict;
+use warnings;
+
+use CLI::Helpers qw(:all);
+use Git::Repository;
+
+# VERSION
+
+1;
