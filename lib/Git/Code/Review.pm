59806c0f6e7f00aa00fc1db826140622c883b8fc
Brad Lhotsky <brad.lhotsky@booking.com>
2014-03-06 22:19:32 +0100

    Pull in Getopt::Long so we can start parsing options from the cli

--- lib/Git/Code/Review.pm
+++ lib/Git/Code/Review.pm
@@ -3,9 +3,10 @@ package Git::Code::Review;
 use strict;
 use warnings;
 
+# VERSION
+
+use Getopt::Long qw(:config pass_through);
 use CLI::Helpers qw(:all);
 use Git::Repository;
-
-# VERSION
 
 1;
